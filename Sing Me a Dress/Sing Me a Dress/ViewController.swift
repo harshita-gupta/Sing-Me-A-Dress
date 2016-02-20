56a8635119a41704f8954e57cef7b2cf76a8da38
harshita-gupta <gupta.harshita.001@gmail.com>
2016-02-20 14:15:56 -0500

    initial commit

---
+++ Sing Me a Dress/Sing Me a Dress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sing Me a Dress
+//
+//  Created by Harshita Gupta on 2/20/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sing Me a Dress/Sing Me a DressTests/Sing_Me_a_DressTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sing_Me_a_DressTests.swift
+//  Sing Me a DressTests
+//
+//  Created by Harshita Gupta on 2/20/16.
+//
+//
+
+import XCTest
+@testable import Sing_Me_a_Dress
+
+class Sing_Me_a_DressTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
