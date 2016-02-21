34b88ac4309e57cd959fb98308821d2c9e511d98
harshita-gupta <gupta.harshita.001@gmail.com>
2016-02-20 23:12:38 -0500

    Added Firebase CocoaPods

---
+++ Sing Me a Dress/Pods/Target Support Files/Pods-Sing Me a Dress/Pods-Sing Me a Dress-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sing_Me_a_Dress : NSObject
+@end
+@implementation PodsDummy_Pods_Sing_Me_a_Dress
+@end
---
+++ Sing Me a Dress/Pods/Target Support Files/Pods-Sing Me a DressUITests/Pods-Sing Me a DressUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sing_Me_a_DressUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Sing_Me_a_DressUITests
+@end
